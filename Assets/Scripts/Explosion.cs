703d367243a97a9603e2638f02273be7f5a98686
alelievr <alelievr@student.42.fr>
2017-10-27 14:00:56 +0200

    Added tables and explosion

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Explosion : MonoBehaviour
+{
+	public float		radius = 10;
+	public GameObject	explosionPrefab;
+	public float		explosionPower = 10;
+	public bool			explode = false;
+
+	List< Rigidbody > objectsInsideExplosion = new List< Rigidbody >();
+
+	void Start ()
+	{
+	}
+	
+	void Update ()
+	{
+		if (explode || Input.GetKeyDown(KeyCode.E))
+			Explode();
+		
+		if (Input.GetKeyDown(KeyCode.R))
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Explodable")
+			objectsInsideExplosion.Add(other.GetComponent< Rigidbody >());
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Explodable")
+			objectsInsideExplosion.Remove(other.GetComponent< Rigidbody >());
+	}
+
+	void Explode()
+	{
+		Vector3 explosionPosition = new Vector3(transform.position.x, 0, transform.position.z);
+		GameObject explosion = GameObject.Instantiate(explosionPrefab, explosionPosition, Quaternion.identity) as GameObject;
+		Destroy(explosion, 10);
+
+		foreach (var obj in objectsInsideExplosion)
+		{
+			Vector3 direction = (obj.transform.position - transform.position);
+			float dist = direction.magnitude;
+			obj.AddForce(direction * (1 / (dist * dist + .1f)) * explosionPower, ForceMode.Impulse);
+		}
+		explode = false;
+	}
+}
